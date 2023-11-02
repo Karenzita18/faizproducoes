ae4c8ff575f2a3b72c5fede64f92e288b2c3127a
karen <karensouza8@hotmail.com>
2023-11-02 10:53:14 -0300

    K: update services

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,64 @@
+import HeroCommon from "@/components/common/servicos/HeroCommon";
+import FeedServices from "@/components/common/FeedFaiz/FeedServices";
+
+export default function Page() {
+  return (
+    <main className="mt-20">
+        <HeroCommon/>
+        <FeedServices/>
+    </main>
+  );
+}
+
+export const metadata = {
+  title:
+    "Serviços | Faiz Produções",
+  description: "",
+  keywords: [
+    "Faiz produções",
+    "tecnologia",
+    "Publicidade",
+    "Produção",
+    "Assessoria",
+    "Projetos",
+    "AudioVisual",
+    "Serviços",
+  ],
+  alternates: {
+    canonical: "faizproducoes.com",
+    languages: {
+      "pt-BR": "/pt-BR",
+    },
+  },
+  // openGraph: {
+  // 	images: [
+  // 		{
+  // 			url: "",
+  // 			width: 2048,
+  // 			height: 449,
+  // 		},
+  // 	],
+  // },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/img/favicon.png",
+    shortcut: "/img/favicon.png",
+    apple: "/img/favicon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/img/favicon.png",
+    },
+  },
+};
