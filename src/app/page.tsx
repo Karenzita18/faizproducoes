14728d516ef1584ba43bddc2dc87fda493c922cd
karen <karensouza8@hotmail.com>
2023-11-02 10:58:54 -0300

    K: update clientes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import FeedServices from "@/components/common/FeedFaiz/FeedServices";
 import FeedAbout from "@/components/common/FeedFaiz/FeedAbout";
-//import FeedClientes from "@/components/common/FeedFaiz/FeedClientes";
+import FeedClientes from "@/components/common/FeedFaiz/FeedClientes";
 //import FeedPortifolio from "@/components/common/FeedFaiz/FeedPortifolio";
 
 export default function Home() {
   return (
     <main>
       <FeedAbout/>
-      
+      <FeedClientes/>
       <FeedServices/>
     </main>
   );
---
+++ src/components/common/FeedFaiz/FeedClientes.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+const FeedClientes = () => {
+	return (
+		<section>
+			<div className="row">
+				<div className="container">
+					<h1 className="text-4xl font-semibold text-center mb-3">
+						Clientes
+					</h1>
+					<p className="text-center text-lg mb-6 text-faiz-100">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+					</p>
+					<Swiper
+						slidesPerView={1}
+						loop={true}
+						autoplay={true}
+						pagination={{
+							clickable: true,
+						}}
+						navigation={false}
+						modules={[Navigation]}
+						breakpoints={{
+							640: {
+								slidesPerView: 2,
+							},
+							768: {
+								slidesPerView: 4,
+							},
+							1024: {
+								slidesPerView: 7,
+							},
+						}}
+						className=""
+					>
+						<SwiperSlide className="relative m-auto">
+							<div className="w-1/2 flex m-auto items-center justify-center h-full">
+								<img 
+									src="/img/empresas.png" 
+									alt="" 
+									title="" 
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="w-3/4 m-auto flex mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt="/"
+									title="/"
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="w-1/2 flex m-auto mx-5 items-center justify-center h-full">
+								<img 
+									src="/img/empresas.png" 
+									alt="" 
+									title="" 
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="relative m-auto">
+							<div className="flex my-auto mx-5 items-center justify-center h-full">
+								<img
+									src="/img/empresas.png"
+									alt=""
+									title=""
+								/>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default FeedClientes;
