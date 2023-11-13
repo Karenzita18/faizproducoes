d6ba15e91e0722becb4c0fb8b48723bc0b208c68
karen <karensouza8@hotmail.com>
2023-11-13 20:01:52 -0300

    K: update home

--- src/components/common/FeedFaiz/FeedQuadrado.tsx
+++ src/components/common/FeedFaiz/FeedQuadrado.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const QuadradosColoridos = () => {
-  const cores = ['#312782', '#e20613', '#e61b72', '#29a600', '#169cd8', 'yellow', 'white'];
+  const cores = ['#312782', '#e20613', '#e61b72', '#29a600', '#169cd8', 'yellow', '#D3D3D3'];
 
   return (
-    <div style={{ display: 'flex', overflowX: 'auto' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
       {cores.map((cor, index) => (
         <div
           key={index}
           style={{
-            flex: '0 0 auto',
-            width: '14%',
+            flex: 1,
             height: '8px',
             backgroundColor: cor,
             //margin: '5px',
