6a9ddfc6e9705e16aef3afafb09e2e1ca4287443
Karen Souza <91435396+Karenzita18@users.noreply.github.com>
2023-12-23 18:19:59 -0300

    K: update carrossel

--- src/components/common/FeedFaiz/FeedPortifolio.tsx
+++ src/components/common/FeedFaiz/FeedPortifolio.tsx
@@ -14,22 +14,22 @@ const FeedPortifolio= () => {
 					<img src="https://via.placeholder.com/700x300" alt="" />
 				</div>
 				<div className="animate-fadeInLeft animate-delay-200 col-span-2">
-				    <h1 className="text-2xl md:text-3xl mb-4 font-bold justify-center text-center text-black">
-                        Portfólio
+				  <h1 className="mt-2 md:mt-0 text-2xl md:text-3xl mb-4 font-bold justify-center text-center text-black">
+              Portfólio
 					</h1>
 					<p className="text-lg md:text-md mb-4  text-center text-black"> 
-                        Bem-vindo à Faiz Produções, onde transformamos ideias em experiências visuais memoráveis. Explore nosso portfólio e descubra o poder da narrativa audiovisual.
+            Bem-vindo à Faiz Produções, onde transformamos ideias em experiências visuais memoráveis. Explore nosso portfólio e descubra o poder da narrativa audiovisual.
 					</p>
-                    <div className="flex place-content-center">
-                        <Link href="https://www.behance.net/faizproducoes" target="_blank">
-                            <button className="font-bold rounded-full outline-none text-white bg-faiz-100 hover:bg-quadrado-100/80 px-5 py-2">
-                                Acesse o portfólio
-                                <span className="inline-block -mb-1 ml-2 text-white">
-                                    < BsArrowRight/>
-                                </span>
-                            </button>
-                        </Link>
-                    </div>
+          <div className="flex place-content-center">
+            <Link href="https://www.behance.net/faizproducoes" target="_blank">
+                <button className="font-bold rounded-full outline-none text-white bg-faiz-100 hover:bg-quadrado-100/80 px-5 py-2">
+                  Acesse o portfólio
+                  <span className="inline-block -mb-1 ml-2 text-white">
+                    < BsArrowRight/>
+                  </span>
+                </button>
+              </Link>
+          </div>
 				</div>
 			</div>
 		</div>
