60cd55fbf161282683beef0ac407cff5d9f88713
Karen Souza <91435396+Karenzita18@users.noreply.github.com>
2023-12-23 18:13:07 -0300

    K: update carrossel

--- src/components/common/FeedFaiz/FeedClientes.tsx
+++ src/components/common/FeedFaiz/FeedClientes.tsx
@@ -59,7 +59,30 @@ const FeedClienteTeste = () => {
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
                     <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/camara-municipal.webp" alt="Câmara Municipal" />
+                      <Image
+                        src="/img/Clientes-novos/camara-municipal.webp" 
+                        alt="Câmara Municipal"
+                        width={500}
+                        height={344}
+                       />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <img
+                      src="https://via.placeholder.com/500x344" 
+                      alt="Copeno" 
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/carreiro-capataz.webp" 
+                        alt="Carreiro Capataz"
+                        width={500}
+                        height={344}
+                       />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
@@ -69,7 +92,112 @@ const FeedClienteTeste = () => {
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
                     <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/carreiro-capataz.webp" alt="Carreiro Capataz" />
+                      <Image
+                        src="/img/Clientes-novos/hagens.webp" 
+                        alt="Hagens" 
+                        width={500}
+                        height={344}
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/twotigers.webp" 
+                        alt="Twotigers" 
+                        width={500}
+                        height={344}
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/ville-ambientes.webp"
+                        alt="Ville Ambientes"
+                        width={500}
+                        height={344}
+                        />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/puc-campinas.webp" 
+                        alt="Puc Campinas" 
+                        width={500}
+                        height={344}
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/blue-trading.webp" 
+                        alt="Blue trading" 
+                        width={500}
+                        height={344}
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/wa-noticias.webp" 
+                        alt="WA Notícias"
+                        width={500}
+                        height={344}
+                       />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image 
+                        src="/img/Clientes-novos/tv-tododia.webp" 
+                        alt="TV Todo Dia"
+                        width={500}
+                        height={344}
+                       />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/uptv.webp" 
+                        alt="UP TV" 
+                        width={500}
+                        height={344}
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/radio-paraiso.webp"
+                        alt="Rádio Paraíso"
+                        width={500}
+                        height={344} 
+                       />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image 
+                        src="/img/Clientes-novos/nova-alianca.webp" 
+                        alt="Nova Aliança"
+                        width={500}
+                        height={344} 
+                      />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide className="py-3">
+                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
+                      <Image
+                        src="/img/Clientes-novos/copeno.webp" 
+                        alt="Copeno" 
+                        width={500}
+                        height={344} 
+                      />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
@@ -79,72 +207,22 @@ const FeedClienteTeste = () => {
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
                     <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/hagens.webp" alt="Hagens" />
+                      <Image
+                        src="/img/Clientes-novos/igreja-conviva.webp" 
+                        alt="Conviva"
+                        width={500}
+                        height={344} 
+                       />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
                     <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/twotigers.webp" alt="Twotigers" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/ville-ambientes.webp" alt="Ville Ambientes" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/puc-campinas.webp" alt="Puc Campinas" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/blue-trading.webp" alt="Blue trading" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/wa-noticias.webp" alt="WA Notícias" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/tv-tododia.webp" alt="TV Todo Dia" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/uptv.webp" alt="UP TV" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/radio-paraiso.webp" alt="Rádio Paraíso" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/nova-alianca.webp" alt="Nova Aliança" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/copeno.webp" alt="Copeno" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="https://via.placeholder.com/500x344" alt="Copeno" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/igreja-conviva.webp" alt="Conviva" />
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="py-3">
-                    <div className="max-w-xs mx-auto shadow-md rounded-tr-3xl rounded-bl-3xl rounded overflow-hidden hover:-translate-y-2 transition duration-500 group">
-                      <img src="/img/Clientes-novos/igreja-nazareno.webp" alt="Igreja Nazareno" />
+                      <Image
+                        src="/img/Clientes-novos/igreja-nazareno.webp" 
+                        alt="Igreja Nazareno" 
+                        width={500}
+                        height={344} 
+                      />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="py-3">
@@ -168,7 +246,6 @@ const FeedClienteTeste = () => {
                     </div>
                 </div>
             </div>
-
           </div>
         </div>
       </section>
